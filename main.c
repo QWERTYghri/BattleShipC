ea50729fddc9b95f82eb593ba25ecf8125552a97
Qwerty-ghri <nitwitghfj17717@protonmail.com>
2022-06-06 05:02:16 -0700

    main.c edits again
    Everything is working except for the initGrid graph labels because I don't know how I would be able to do them

--- main.c
+++ main.c
@@ -14,6 +14,7 @@
 
 
 /* Definitions */
+
 #define MAX_GRID	( ( int32_t ) 8 )
 #define SHOW_NUM	( ( int32_t ) 0 )
 #define MAX_TRIES	( ( int32_t ) 4 )
@@ -45,6 +46,17 @@ int32_t getStrNum ( void )	/* Func to get an int from stdin */
 	return tempVal;
 }
 
+/* sHITTY FUCKING CODE go kys */
+char* retIntStrConv ( uint32_t val )
+{
+	char* str = ( char* ) malloc ( sizeof ( MAX_STR_IN ) ); /* Someone could make some gdb breakpoint
+								   and do some funny stuff by altering
+								   the return perhaps.*/	
+	sprintf ( str, "%d", val );
+
+	return str;
+}
+	
 void initGrid ( void )		/* initialize the gameGrid */
 {
 	uint32_t y, x;
@@ -53,13 +65,14 @@ void initGrid ( void )		/* initialize the gameGrid */
 	{
 		for ( x = 0; x < MAX_GRID; x++ )
 		{
+			/* kys wtf is this code */
 			if ( y == 0 )
 			{
-				/* A bit of a problem here, I have to cast long to str and then 
-				 * add it to the strng. But snprintf or any other can't send the
-				 * formatted string to the two dimensional array. I can make an
-				 * array space but it probably wouldn't work
-				 */
+				gameGrid[y][x] = retIntStrConv ( y );
+			}
+			else if ( x == 0 )
+			{
+				gameGrid[y][x] = retIntStrConv ( x );
 			}
 		}
 	}
@@ -85,14 +98,14 @@ int32_t retRandRng ( void ) /* returns rand num from 1 - MAX_GRID */
 	return LOW_RAND + rand() / (RAND_MAX / (MAX_GRID - LOW_RAND + 1) + 1);	
 }
 
-void clsBoard ( void )		/* Clear the board */
+void clsGrid ( void )		/* Clear the board */
 {
 	uint32_t y, x;
 
 	for ( y = 1; y < MAX_GRID; y++ )
 	{
 		for ( x = 1; x < MAX_GRID; x++ )
-			gameGrid[y][x] = "X";
+			gameGrid[y][x] = "O";
 	}
 }
 
@@ -106,15 +119,16 @@ int main ( void )
 	srand ( time ( NULL ) );
 	printf ( "%s\n\n", intro );
 	
-	initGrid ();
-
 	while ( 1 ) /* game loop */
 	{
+		initGrid ();
+		clsGrid ();
+
 		rand1 = retRandRng ();
 		rand2 = retRandRng ();
 		
 		
-		while ( tries >= 0 )
+		while ( tries > 0 )
 		{
 			printGrid ();
 			printf ( "\n%d %d\n", rand1, rand2 );
@@ -122,7 +136,7 @@ int main ( void )
 			inp1 = getStrNum ();
 			inp2 = getStrNum ();
 
-			gameGrid[inp1][inp2] = "X";
+			gameGrid[inp2][inp1] = "X";
 
 			if ( inp1 == rand1 && inp2 == rand2 )
 			{
@@ -132,18 +146,17 @@ int main ( void )
 				break;
 			}
 			else
-			{
-				score--;
 				printf ( "\n\nYou Lost\n" );
-			}
+
 			tries--;
+
+			if ( !tries )
+				printf ( "\n\nFailed -5" );
 		}
 
-		printf ( "GAME ENDED\n\n" );
+		printf ( "GAME ENDED\n\nScore : %d\n\n", score );
 
-		clsBoard ();
 		tries = MAX_TRIES;
-		score = 0;
 
 	}
 
